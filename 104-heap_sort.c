3d24315534c3940c3849fa77c7ee16117f29cd35
shrouk_ouda <shroukouda2@gmail.com>
2024-02-20 16:57:12 -0500

    commit

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,4 +1,19 @@
 #include "sort.h"
+#include "sort.h"
+/**
+ * _swap - check the code.
+ * @a:---
+ * @b:---
+ * Return: ----.
+ */
+void _swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
 /**
  * heapify - check the code.
  * @array:---
